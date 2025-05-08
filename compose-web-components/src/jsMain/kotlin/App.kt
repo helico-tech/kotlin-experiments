bafdf9b16b3cc119ebcde69a573cc663c3b1d76e
Arjan van Wieringen <git@helico-tech.nl>
2025-05-08 11:04:42 +0200

    attributeChanged working

--- compose-web-components/src/jsMain/kotlin/App.kt
+++ compose-web-components/src/jsMain/kotlin/App.kt
@@ -1,3 +1,5 @@
+import js.core.JsAny
+import js.temporal.Duration
 import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 import org.w3c.dom.Element
@@ -8,8 +10,21 @@ import web.components.customElements
 import web.dom.document
 import web.html.HTMLElement
 import web.html.HtmlTagName
+import web.timers.setInterval
+import kotlin.random.Random
+import kotlin.time.Duration.Companion.milliseconds
 
-class MyWebComponent : HTMLElement(), CustomElement.WithConnectedCallback {
+class MyWebComponent : HTMLElement(), CustomElement.WithConnectedCallback, CustomElement.WithAttributeChangedCallback {
+
+    companion object {
+        val observedAttributes = arrayOf("counter")
+
+        fun register() {
+            MyWebComponent::class.js.asDynamic().observedAttributes = observedAttributes
+            customElements.define(HtmlTagName("my-web-component"), MyWebComponent::class.js)
+        }
+    }
+
     override fun connectedCallback() {
         val shadow = this.attachShadow(ShadowRootInit(mode = ShadowRootMode.open))
         val root = document.createElement("div")
@@ -22,8 +37,22 @@ class MyWebComponent : HTMLElement(), CustomElement.WithConnectedCallback {
             Text("Hello World!")
         }
     }
+
+    override fun attributeChangedCallback(name: String, oldValue: JsAny?, newValue: JsAny?) {
+        println("Attribute changed: $name, $oldValue, $newValue")
+    }
 }
 
 fun main() {
-    customElements.define(HtmlTagName("my-web-component"), MyWebComponent::class.js)
+    MyWebComponent.register()
+
+    val myElement = document.createElement("my-web-component")
+    myElement.setAttribute("counter", "1")
+
+    document.body.appendChild(myElement)
+
+    setInterval(timeout = 1000.milliseconds) {
+        myElement.setAttribute("counter", Random.nextInt(1, 100).toString())
+        println("Interval!")
+    }
 }
