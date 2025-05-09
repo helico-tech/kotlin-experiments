bbb956e44703a364b1226502f3c512d29b1a3372
Arjan van Wieringen <git@helico-tech.nl>
2025-05-09 20:14:35 +0200

    WIP

--- compose-web-components/src/jsMain/kotlin/TimerWebComponent.kt
+++ compose-web-components/src/jsMain/kotlin/TimerWebComponent.kt
@@ -1,8 +1,20 @@
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import js.core.JsAny
+import kotlinx.coroutines.awaitAnimationFrame
+import kotlinx.coroutines.delay
+import org.jetbrains.compose.web.dom.H1
+import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.renderComposable
 import web.components.CustomElement
 import web.components.ShadowRoot
 import web.components.ShadowRootInit
 import web.components.ShadowRootMode
+import web.cssom.attr
 import web.dom.document
 import web.html.HTMLElement
 
@@ -11,21 +23,53 @@ import web.html.HTMLElement
 @JsName("TimerWebComponent")
 class TimerWebComponent : HTMLElement(), CustomElement.WithCallbacks {
 
-    val shadow: ShadowRoot = this.attachShadow(ShadowRootInit(mode = ShadowRootMode.closed))
+    companion object {
+        val attributes = arrayOf(Attributes.Time)
 
-    override fun connectedCallback() {
-        document.createElement("h1").apply {
-            innerText = "Hello from WebComponent"
-            shadow.appendChild(this)
+        @OptIn(ExperimentalJsStatic::class)
+        @JsStatic()
+        @JsName("observedAttributes")
+        val staticObservedAttributes = attributes.map { it.name }.toTypedArray()
+
+        object Attributes {
+            val Time = ObservedAttribute("time", 0, { it?.toString()?.toIntOrNull() ?: 0 })
         }
     }
+
+    val shadow: ShadowRoot = this.attachShadow(ShadowRootInit(mode = ShadowRootMode.closed))
+    val observedAttributes = ObservedAttributes(*TimerWebComponent.attributes)
+
+    override fun connectedCallback() {
+        val root = document.createElement("main").apply {
+            shadow.appendChild(this)
+        }
+
+        renderComposable(root as org.w3c.dom.HTMLElement) {
+            var currentTime by remember { mutableStateOf(0) }
+            val initialTime by observedAttributes[Attributes.Time].collectAsState()
+
+            LaunchedEffect(initialTime) {
+                currentTime = initialTime
+                while (true) {
+                    currentTime -= 1
+                    currentTime = currentTime.coerceAtLeast(0)
+                    delay(1000)
+                }
+            }
+
+            H1 { Text("Time: ${currentTime}") }
+        }
+    }
+
+    override fun attributeChangedCallback(name: String, oldValue: JsAny?, newValue: JsAny?) {
+        observedAttributes.attributeChangedCallback(name, oldValue, newValue)
+    }
+
     override fun disconnectedCallback() {
         println("TimerWebComponent disconnected!")
     }
+
     override fun adoptedCallback() {
         println("TimerWebComponent adopted!")
-    }
-    override fun attributeChangedCallback(name: String, oldValue: JsAny?, newValue: JsAny?) {
-        println("TimerWebComponent attributeChangedCallback $name $oldValue $newValue")
     }
 }
